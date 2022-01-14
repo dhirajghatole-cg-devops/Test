755145b502ef6570fcd4c11aaf606f9b368716fd
dghatole <dghatole@LIN51013034.corp.capgemini.com>
2022-01-14 13:35:15 +0530

    Test

---
+++ demo-2/src/main/java/RestSpringController/ConsumeController.java
@@ -0,0 +1,13 @@
+package RestSpringController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ConsumeController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+ 
+		return "Dhiraj"; 
+	}
+}
